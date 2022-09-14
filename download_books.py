b619621009b89be09a527e1245bb3eeab6583375
Aleksy <aslepaugo@gmail.com>
2022-09-14 08:35:58 +0200

    modify exception handler

--- download_books.py
+++ download_books.py
@@ -1,5 +1,7 @@
 import argparse
 import requests
+import sys
+import time
 
 from bs4 import BeautifulSoup
 from pathlib import Path
@@ -72,13 +74,17 @@ if __name__ == '__main__':
     parser.add_argument('--start_id', help='Start ID for book', type=int, default=1)
     parser.add_argument('--end_id', help='End ID for book', type=int, default=10)
     args = parser.parse_args()
-    for book_id in range(args.start_id, args.end_id + 1):
-        response = requests.get(f'https://tululu.org/b{book_id}/')
-        response.raise_for_status()
+    for book_id in range(args.start_id, args.end_id + 1):     
         try:
+            response = requests.get(f'https://tululu.org/b{book_id}/')
+            response.raise_for_status()
             check_for_redirect(response)
-        except requests.exceptions.HTTPError:
+            book = parse_book_page(response.text)
+            download_txt(f'https://tululu.org/txt.php?id={book_id}', f"{book_id}. {book['title']}", 'books')
+            download_image(book['image_url'], 'images')
+        except requests.exceptions.HTTPError as err:
+            print(err, file=sys.stderr)
             continue        
-        book = parse_book_page(response.text)
-        download_txt(f'https://tululu.org/txt.php?id={book_id}', f"{book_id}. {book['title']}", 'books')
-        download_image(book['image_url'], 'images')
+        except requests.exceptions.ConnectionError as err:
+            print(err, file=sys.stderr)
+            time.sleep(5)
