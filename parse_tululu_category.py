a2747be75a3afc176d3e25373b2fc402277a2419
Aleksy <aslepaugo@gmail.com>
2022-11-21 20:10:03 +0100

    add pagination.

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -5,13 +5,16 @@ from urllib.parse import urljoin
 
 
 base_url = "https://tululu.org"
-scince_fiction_url = "https://tululu.org/l55/"
-response = requests.get(scince_fiction_url)
-response.raise_for_status()
-content = response.text
-soup = BeautifulSoup(content, 'lxml')
-book_cards = soup.find('div', {'id':'content'}).find_all('table')
-for book_card in book_cards:
-    book_link = book_card.find('a')['href']
-    url = urljoin(base_url, book_link)
-    print(url)
+scince_fiction_url = "https://tululu.org/l55"
+max_page = 10
+
+for page in range(1, max_page + 1):
+    response = requests.get(f"{scince_fiction_url}/{page}")
+    response.raise_for_status()
+    content = response.text
+    soup = BeautifulSoup(content, 'lxml')
+    book_cards = soup.find('div', {'id':'content'}).find_all('table')
+    for book_card in book_cards:
+        book_link = book_card.find('a')['href']
+        url = urljoin(base_url, book_link)
+        print(url)
