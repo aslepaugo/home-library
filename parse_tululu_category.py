05ded73ba3dbe5e838dbed8d69d4f26749a0d59c
Aleksy <aslepaugo@gmail.com>
2022-11-27 09:38:21 +0100

    add parameters for start and end page.

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import re
 import requests
@@ -10,38 +11,53 @@ from urllib.parse import urljoin
 import download_books
 
 
-base_url = "https://tululu.org"
-scince_fiction_url = "https://tululu.org/l55"
-max_page = 1
-books = []
-for page in range(1, max_page + 1):
-    response = requests.get(f"{scince_fiction_url}/{page}")
+if __name__ == '__main__':
+    base_url = "https://tululu.org"
+    scince_fiction_url = "https://tululu.org/l55"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start_page", type=int, default=1,
+                        help="Identify from which page you would like to start.")
+    parser.add_argument("--end_page", type=int,
+                        help="Identify on which page you would like to end. (end page won't be parsed)")
+    args = parser.parse_args()
+    start_page = args.start_page
+    end_page = args.end_page
+    response = requests.get(f"{scince_fiction_url}")
     response.raise_for_status()
-    content = response.text
-    soup = BeautifulSoup(content, 'lxml')
-    book_links = [book_link_tag['href'] for book_link_tag in soup.select('.bookimage a')]
-    for book_link in book_links:
-        book_url = urljoin(base_url, book_link)
-        book_id = re.findall(r'\d+', book_link)[0]
-        try:
-            response = requests.get(book_url)
-            response.raise_for_status()
-            download_books.check_for_redirect(response)
-            book = download_books.parse_book_page(response.text)
-            if not book['txt_link']:
-                print(f'There is no TXT book for the link {book_url}')
+    soup = BeautifulSoup(response.text, 'lxml')
+    max_page = int(soup.select_one("p.center > :last-child").text)
+    if not end_page or end_page <= start_page or end_page > max_page + 1:
+        end_page = max_page + 1
+    books = []
+    for page in range(start_page, end_page):
+        response = requests.get(f"{scince_fiction_url}/{page}")
+        response.raise_for_status()
+        content = response.text
+        soup = BeautifulSoup(content, 'lxml')
+        book_links = [book_link_tag['href'] for book_link_tag in soup.select('.bookimage a')]
+        for book_link in book_links:
+            book_url = urljoin(base_url, book_link)
+            book_id = re.findall(r'\d+', book_link)[0]
+            try:
+                response = requests.get(book_url)
+                response.raise_for_status()
+                download_books.check_for_redirect(response)
+                book = download_books.parse_book_page(response.text)
+                if not book['txt_link']:
+                    print(f'There is no TXT book for the link {book_url}')
+                    continue
+                books.append(book)
+                download_books.download_txt('https://tululu.org/txt.php', book_id, f"{book_id}. {book['title']}", 'books')
+                download_books.download_image(urljoin(book_url, book['image_path']), 'images')
+            except requests.exceptions.HTTPError as err:
+                print(err, file=sys.stderr)
+                continue        
+            except requests.exceptions.ConnectionError as err:
+                print(err, file=sys.stderr)
+                time.sleep(5)
                 continue
-            books.append(book)
-            download_books.download_txt('https://tululu.org/txt.php', book_id, f"{book_id}. {book['title']}", 'books')
-            download_books.download_image(urljoin(book_url, book['image_path']), 'images')
-        except requests.exceptions.HTTPError as err:
-            print(err, file=sys.stderr)
-            continue        
-        except requests.exceptions.ConnectionError as err:
-            print(err, file=sys.stderr)
-            time.sleep(5)
-            continue
 
 
-with open("books.json", "w", encoding="utf8") as file:
-    json.dump(books, file, ensure_ascii=False)
+    with open("books.json", "w", encoding="utf8") as file:
+        json.dump(books, file, ensure_ascii=False)
