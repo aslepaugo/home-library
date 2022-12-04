be55c6892537b4a384c6f8a011ad9ae3fc0e7a46
Aleksy <aslepaugo@gmail.com>
2022-12-04 09:34:38 +0100

    decompose code to functions.

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -11,10 +11,7 @@ from urllib.parse import urljoin
 import download_books
 
 
-if __name__ == '__main__':
-    base_url = "https://tululu.org"
-    scince_fiction_url = "https://tululu.org/l55"
-
+def create_parser():
     parser = argparse.ArgumentParser(
         description = "Script to download books from https://tululu.org by predefined category (SciFi by default)."
     )
@@ -30,50 +27,68 @@ if __name__ == '__main__':
                         help="Do not download books.")
     parser.add_argument("--json_path", type=str, default="books.json",
                         help="Path to json-file with book descriptions (default: books.json).")
-    args = parser.parse_args()
-    start_page = args.start_page
-    end_page = args.end_page
-    response = requests.get(f"{scince_fiction_url}")
+    return parser
+
+
+def get_end_page(url, end_page=None):
+    response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     max_page = int(soup.select_one("p.center > :last-child").text)
     if not end_page or end_page <= start_page or end_page > max_page + 1:
         end_page = max_page + 1
-    books = []
+    return end_page
+
+
+def get_book_links(url, start_page, end_page):
+    book_links = []
     for page in range(start_page, end_page):
-        response = requests.get(f"{scince_fiction_url}/{page}")
+        response = requests.get(f"{url}/{page}")
         response.raise_for_status()
         content = response.text
         soup = BeautifulSoup(content, 'lxml')
-        book_links = [book_link_tag['href'] for book_link_tag in soup.select('.bookimage a')]
-        for book_link in book_links:
-            book_url = urljoin(base_url, book_link)
-            book_id = re.findall(r'\d+', book_link)[0]
-            try:
-                response = requests.get(book_url)
-                response.raise_for_status()
-                download_books.check_for_redirect(response)
-                book = download_books.parse_book_page(response.text)
-                if not book['txt_link']:
-                    print(f'There is no TXT book for the link {book_url}')
-                    continue
-                books.append(book)
-                if not args.skip_txt:
-                    download_books.download_txt(
-                        'https://tululu.org/txt.php', 
-                        book_id, 
-                        f"{book_id}. {book['title']}", 
-                        args.dest_folder
-                    )
-                if not args.skip_imgs:
-                    download_books.download_image(urljoin(book_url, book['image_path']), 'images')
-            except requests.exceptions.HTTPError as err:
-                print(err, file=sys.stderr)
-                continue        
-            except requests.exceptions.ConnectionError as err:
-                print(err, file=sys.stderr)
-                time.sleep(5)
+        book_links += [book_link_tag['href'] for book_link_tag in soup.select('.bookimage a')]
+    return book_links
+
+
+if __name__ == '__main__':
+    base_url = "https://tululu.org"
+    scince_fiction_url = "https://tululu.org/l55"
+    parser = create_parser()
+    args = parser.parse_args()
+    start_page = args.start_page
+    end_page = get_end_page(url=scince_fiction_url, end_page=args.end_page)
+    books = []
+
+    book_links = get_book_links(url=scince_fiction_url, start_page=start_page, end_page=end_page)
+    for book_link in book_links:
+        book_url = urljoin(base_url, book_link)
+        book_id = re.findall(r'\d+', book_link)[0]
+        try:
+            response = requests.get(book_url)
+            response.raise_for_status()
+            download_books.check_for_redirect(response)
+            book = download_books.parse_book_page(response.text)
+            if not book['txt_link']:
+                print(f'There is no TXT book for the link {book_url}')
                 continue
+            books.append(book)
+            if not args.skip_txt:
+                download_books.download_txt(
+                    'https://tululu.org/txt.php', 
+                    book_id, 
+                    f"{book_id}. {book['title']}", 
+                    args.dest_folder
+                )
+            if not args.skip_imgs:
+                download_books.download_image(urljoin(book_url, book['image_path']), 'images')
+        except requests.exceptions.HTTPError as err:
+            print(err, file=sys.stderr)
+            continue        
+        except requests.exceptions.ConnectionError as err:
+            print(err, file=sys.stderr)
+            time.sleep(5)
+            continue
 
 
     with open(args.json_path, "w", encoding="utf8") as file:
